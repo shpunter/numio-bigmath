0abfa44baf2e0339d3cd324bde8515c389eda07d
Oleksandr <shpunterman@gmail.com>
2024-12-15 13:06:10 +0100

    fix lint warnin

--- lab/add/v3/index.ts
+++ lab/add/v3/index.ts
@@ -1,8 +1,8 @@
 import { addIntPart, splitFn } from "./utils.ts";
 
 export const addV3 = (num1: string, num2: string) => {
-  const [int1, frac1] = splitFn(num1);
-  const [int2, frac2] = splitFn(num2);
+  const [int1] = splitFn(num1);
+  const [int2] = splitFn(num2);
 
   const result = addIntPart(int1, int2);
   let str = "";

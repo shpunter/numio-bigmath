1695d141c12f34e7fb82d38c7657af8db19d2cff
Oleksandr <shpunterman@gmail.com>
2024-12-23 19:57:49 +0100

    add package.json

--- index.ts
+++ index.ts
@@ -3,6 +3,3 @@ import { mul } from "./mul/index.ts";
 import { sub } from "./sub/index.ts";
 
 export { add, mul, sub };
-
-console.log(mul("0.00000000000000000000000000000000000000000000000000000000000000000009",
-   "0.000000002"));

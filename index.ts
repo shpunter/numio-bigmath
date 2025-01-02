1642e1ab8166333c84a7a0a221963ba159609903
Oleksandr <shpunterman@gmail.com>
2025-01-02 11:29:38 +0100

    add export

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import { add } from "./src/add/index.ts";
 import { mul } from "./src/mul/index.ts";
 import { sub } from "./src/sub/index.ts";
+import { div } from "./src/div/index.ts";
 
-export { add, mul, sub };
+
+export { add, mul, sub, div };

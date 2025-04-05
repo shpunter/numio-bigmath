217c64348cb9c433a7e15938182216583681fdae
Oleksandr <shpunterman@gmail.com>
2025-04-05 11:52:43 +0200

    fix types

--- src/pipe/main.ts
+++ src/pipe/main.ts
@@ -4,6 +4,8 @@ import { mulRoute } from "../mul/utils.ts";
 import { DEFAULT } from "../shared/constant.ts";
 import { a2s, s2a } from "../shared/utils.ts";
 import { subRoute } from "../sub/utils.ts";
+
+import type { A2S } from "../shared/types.ts";
 import type { InputData } from "../types.ts";
 
 class Pipe {
@@ -11,31 +13,31 @@ class Pipe {
 
   constructor() {}
 
-  add(strs: string[]) {
+  add(strs: string[]): Pipe {
     this.result = addRoute(strs.map((str) => s2a(str)), this.result);
 
     return this;
   }
 
-  sub(strs: string[]) {
+  sub(strs: string[]): Pipe {
     this.result = subRoute(strs.map((str) => s2a(str)), this.result);
 
     return this;
   }
 
-  div(strs: string[], limit = 20) {
+  div(strs: string[], limit = 20): Pipe {
     this.result = divRoute(strs.map((str) => s2a(str)), this.result, limit);
 
     return this;
   }
 
-  mul(strs: string[]) {
+  mul(strs: string[]): Pipe {
     this.result = mulRoute(strs.map((str) => s2a(str)), this.result);
 
     return this;
   }
 
-  calc() {
+  calc(): ReturnType<A2S> {
     const result = a2s(this.result);
     this.result = DEFAULT;
 

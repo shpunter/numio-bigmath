2effe0162a0cd5f784d88410492579ab4b9d76be
Oleksandr <shpunterman@gmail.com>
2025-04-28 21:59:11 +0200

    import methods

--- index.ts
+++ index.ts
@@ -3,30 +3,43 @@ import { mul } from "./src/operations/mul/main.ts";
 import { sub } from "./src/operations/sub/main.ts";
 import { div } from "./src/operations/div/main.ts";
 import { round } from "./src/round/main.ts";
-import { pipe } from "./src/pipe/main.ts";
+import { Pipe } from "./src/pipe/main.ts";
 import { quartile } from "./src/quartile/main.ts";
 import { sort } from "./src/sort/main.ts";
 import { mean } from "./src/mean/main.ts";
-import { isEqual, isLeftGreater, max, min } from "./src/compare/main.ts";
+import {
+  isEqual,
+  isLeftGreater,
+  isLeftGreaterOrEqual,
+  max,
+  min,
+} from "./src/compare/main.ts";
 import { IQR } from "./src/IQR/main.ts";
 import { MAD } from "./src/MAD/main.ts";
 import { sqrt } from "./src/sqrt/main.ts";
+import { cbrt } from "./src/cbrt/main.ts";
+import { abs } from "./src/abs/main.ts";
+import { toBase } from "./src/toBase/main.ts";
 
 export {
+  abs,
   add,
+  cbrt,
   div,
   IQR,
   isEqual,
   isLeftGreater,
+  isLeftGreaterOrEqual,
   MAD,
   max,
   mean,
   min,
   mul,
-  pipe,
+  Pipe,
   quartile,
   round,
   sort,
-  sub,
   sqrt,
+  sub,
+  toBase,
 };
